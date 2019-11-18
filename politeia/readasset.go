29bc5f49a296d27f3451912ffb25800d092e0eec
Josh Rickmar <jrick@companyzero.com>
2019-11-18 14:58:04 -0500

    Add missing file

---
+++ politeia/readasset.go
@@ -0,0 +1,26 @@
+//+build ignore
+
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/decred/politeia/politeiawww/cmd/politeiavoter/sampleconfig"
+)
+
+var assets = map[string][]byte{
+	"sample-politeiavoter.conf":   []byte(sampleconfig.FileContents),
+}
+
+func main() {
+	asset := os.Args[1]
+	contents, ok := assets[asset]
+	if !ok {
+		log.Fatalf("missing asset %q", asset)
+	}
+	_, err := os.Stdout.Write(contents)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
