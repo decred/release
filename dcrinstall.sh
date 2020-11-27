5e6620a531d5cda48f93b16167301e7e2dd98e18
Josh Rickmar <jrick@companyzero.com>
2020-11-27 20:18:28 +0000

    Add script to run dcrinstall with local build

---
+++ dcrinstall.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+TUPLE=$(go version | perl -lane 'print $F[3] =~ s,/,-,r')
+DCRINSTALL=./bin/${TUPLE}/dcrinstall
+
+[ -x ${DCRINSTALL} ] || go run . -dist dcrinstall
+[ -f fake-latest ] || go run . -dist dcrinstall-manifests
+
+exec ${DCRINSTALL} -manifest file://fake-latest -skippgp "$@"
