471e2b784e89f4b29d12ce10baa91d09902b98e4
Josh Rickmar <jrick@companyzero.com>
2020-11-27 19:19:59 +0000

    Use relative paths in faked manifest
    This makes the testing easier to perform on windows.
    
    While here also clean up some parts of the final perl incantation.

--- fakelatest.sh
+++ fakelatest.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 find dist -name '*-manifest.txt' | perl -MDigest::SHA -lne \
-	'print Digest::SHA->new(256)->addfile($_)->hexdigest, "  file://$ENV{PWD}/$_"'
-find thirdparty -type f | perl -MDigest::SHA -MFile::Basename -ne '\
-	chomp;
+	'print Digest::SHA->new(256)->addfile($_)->hexdigest, "  file://$_"'
+find thirdparty -type f | perl -MDigest::SHA -lne '\
 	open (my $f, "<", $_);
 	chomp(my $url = <$f>);
 	local $/;
 	my $contents = <$f>;
 	my $sum = Digest::SHA->new(256)->add($contents)->hexdigest;
-	print "$sum  $url\n";'
+	print "$sum  $url";'
