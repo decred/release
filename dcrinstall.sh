22b76821227411a06607619d6b316df8c10483b3
Josh Rickmar <jrick@companyzero.com>
2020-12-30 16:24:09 +0000

    Force dcrinstall-manifests build if fake-latest is empty

--- dcrinstall.sh
+++ dcrinstall.sh
@@ -6,6 +6,6 @@ TUPLE=$(go version | perl -lane 'print $F[-1] =~ s,/,-,r')
 DCRINSTALL=./bin/${TUPLE}/dcrinstall
 
 [ -x ${DCRINSTALL} ] || go run . -dist dcrinstall
-[ -f fake-latest ] || go run . -dist dcrinstall-manifests
+[ -s fake-latest ] || go run . -dist dcrinstall-manifests
 
 exec ${DCRINSTALL} -manifest file://fake-latest -skippgp "$@"
