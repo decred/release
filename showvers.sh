90dd3e5a3f37c0c8f892ff425cd67e3c6ecdd8ad
Dave Collins <davec@conformal.com>
2024-05-29 13:14:29 -0500

    Remove dup dexctl entry from showvers script.

--- showvers.sh
+++ showvers.sh
@@ -4,7 +4,7 @@ set -e
 
 TUPLE=$(go version | perl -lane 'print $F[-1] =~ s,/,-,r')
 
-for APP in dcrd dcrctl dcrwallet dexc dexcctl dcrlncli dcrlnd dexc-tray dexcctl politeiavoter; do
+for APP in dcrd dcrctl dcrwallet dexc dexcctl dcrlncli dcrlnd dexc-tray politeiavoter; do
 	APP="./bin/${TUPLE}/${APP}"
 	[ -x "${APP}" ] && "${APP}" --version || echo "WARNING: ${APP} is not built"
 done
