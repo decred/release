5e0732e13911d2824b5d315e6841ed64318ca5d6
Dave Collins <davec@conformal.com>
2024-08-30 13:34:27 -0500

    Don't try to show bisonw-tray ver when not Windows.

--- showvers.sh
+++ showvers.sh
@@ -3,8 +3,12 @@
 set -e
 
 TUPLE=$(go version | perl -lane 'print $F[-1] =~ s,/,-,r')
+OS=$(echo $TUPLE | cut -f1 -d-)
 
 for APP in dcrd dcrctl dcrwallet bisonw bisonw-tray bwctl dcrlncli dcrlnd politeiavoter; do
+	if [ "$OS" != "windows" ] && [ "$APP" == "bisonw-tray" ]; then
+		continue
+	fi
 	APP="./bin/${TUPLE}/${APP}"
 	[ -x "${APP}" ] && "${APP}" --version || echo "WARNING: ${APP} is not built"
 done
