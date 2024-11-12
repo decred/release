9dd9a54614eee36560b59c73d2b43e8f9486d2fa
Josh Rickmar <jrick@companyzero.com>
2024-11-12 18:29:58 +0000

    Remove bisonw-tray from showvers.sh

--- showvers.sh
+++ showvers.sh
@@ -3,12 +3,8 @@
 set -e
 
 TUPLE=$(go version | perl -lane 'print $F[-1] =~ s,/,-,r')
-OS=$(echo $TUPLE | cut -f1 -d-)
 
-for APP in dcrd dcrctl dcrwallet bisonw bisonw-tray bwctl dcrlncli dcrlnd politeiavoter; do
-	if [ "$OS" != "windows" ] && [ "$APP" == "bisonw-tray" ]; then
-		continue
-	fi
+for APP in dcrd dcrctl dcrwallet bisonw bwctl dcrlncli dcrlnd politeiavoter; do
 	APP="./bin/${TUPLE}/${APP}"
 	[ -x "${APP}" ] && "${APP}" --version || echo "WARNING: ${APP} is not built"
 done
